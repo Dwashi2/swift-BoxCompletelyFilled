71fbaaa21c137b6f7540ce4ca9c74103aaa490a3
Daniel Washington <danielvertigo@hotmail.com>
2021-06-01 13:19:01 -0300

    Files

--- BoxCompletelyFilled/ViewController.swift
+++ BoxCompletelyFilled/ViewController.swift
@@ -5,6 +5,44 @@
 //  Created by Daniel Washington Ignacio on 01/06/21.
 //
 
+/*
+ Create a function that checks if the box is completely filled with the asterisk symbol *.
+
+ Examples
+
+ completelyFilled([
+   "#####",
+   "#***#",
+   "#***#",
+   "#***#",
+   "#####"
+ ]) ➞ true
+
+ completelyFilled([
+   "#####",
+   "#* *#",
+   "#***#",
+   "#***#",
+   "#####"
+ ]) ➞ false
+
+ completelyFilled([
+   "###",
+   "#*#",
+   "###"
+ ]) ➞ true
+
+ completelyFilled([
+   "##",
+   "##"
+ ]) ➞ true
+ Notes
+
+ Boxes of size n <= 2 are considered automatically filled (see example #4).
+ Empty space will always be a space character (" ").
+ */
+
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,6 +50,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(completelyFilled([
+            "#####",
+            "#***#",
+            "#***#",
+            "#***#",
+            "#####"
+          ]))
+        
+        
+        print(self.completelyFilled([
+            "#####",
+            "#* *#",
+            "#***#",
+            "#***#",
+            "#####"
+          ]))
+        
+        print(self.completelyFilled([
+            "###",
+            "#*#",
+            "###"
+          ]))
+        
+        print(self.completelyFilled([
+            "##",
+            "##"
+          ]) )
+        
+    }
+    
+    func completelyFilled(_ arr: [String]) -> Bool {
+        return !arr.joined().contains(" ")
     }
 
 
